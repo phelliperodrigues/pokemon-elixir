b3ac6c1cf020a776bccd82ddd6b33c3133749f48
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-08-03 21:55:16 -0300

    :sparkles: Create function create_game

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,14 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
 
+  @computer_name "Robotnik"
   def create_player(name, move_avg, move_heal, move_rnd) do
     Player.build(name, move_avg, move_heal, move_rnd)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
