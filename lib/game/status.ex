b546693c3113236bf20cd88125e4af5edbbc5870
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-08-04 12:04:59 -0300

    :sparkles: Print attack message

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -10,4 +10,12 @@ defmodule ExMon.Game.Status do
   def print_wrong_move_message(move) do
     IO.puts("\n======= Invalid move: #{move} =======\n")
   end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n======= The Player attacker the computer dealing #{damage} damage. =======\n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n======= The Computer attacker the player dealing #{damage} damage. =======\n")
+  end
 end
